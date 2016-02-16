e4f004e4017161a39a9f5520377d8b28e1e0a8ca
Connor Grady <connor.grady@gradycapital.us>
2016-02-16 01:18:19 -0600

    Added Our Story;

--- KalashianFamily.Web/Properties/AssemblyInfo.cs
+++ KalashianFamily.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2016.2.5887.334")]
+[assembly: AssemblyVersion("2016.2.5890.336")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
